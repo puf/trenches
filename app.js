326983e50fbcffb490b73fc0477bc902b88cd60f
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2014-08-30 08:10:53 -0400

    Allow editing of cards
    Very basic editing for now. Cleaned up the firebaseSimpleLogin code into
    a service. Dragging a card now makes you the owner.

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 var app = angular.module("trenchesApp", ["ngRoute", "firebase"]);
+app.constant('FBURL', "https://trenches.firebaseio.com/");
 app.config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', { templateUrl: 'boards.html', controller: 'BoardListCtrl' })
 		.when('/board/:boardId', { templateUrl: 'board.html', controller: 'BoardCtrl' })
-		//.when('/q/:qId', { templateUrl: 'question.html', controller: QuestionCtrl })
+		.when('/board/:boardId/card/:cardId', { templateUrl: 'card.html', controller: 'CardCtrl' })
 		.otherwise({ redirectTo: '/' });
 }]);
-app.controller("TrenchesCtrl", function($scope, $firebase, $firebaseSimpleLogin) {
-	var ref = new Firebase("https://trenches.firebaseio.com/");
+app.factory('$firebaseAuth', function($firebase, $firebaseSimpleLogin, FBURL) {
+	return $firebaseSimpleLogin(new Firebase(FBURL));
+});
+app.controller("TrenchesCtrl", function($scope, FBURL, $firebase, $firebaseAuth) {
+	var ref = new Firebase(FBURL);
 	// create an AngularFire reference to the data
 	var sync = $firebase(ref);
 	// download the data into a local object and turn it into a three-way binding
 	sync.$asObject().$bindTo($scope, 'data');
 
-	$scope.auth = $firebaseSimpleLogin(ref); // TODO: store the simpleLogin in a service
+	$scope.auth = $firebaseAuth; // TODO: store the simpleLogin in a service
 });
-app.controller('BoardListCtrl', function BoardListCtrl($scope, $rootScope, $firebase) {
+app.controller('BoardListCtrl', function BoardListCtrl($scope, $rootScope, FBURL, $firebase) {
 	$rootScope.title = 'Trenches';
-	var ref = new Firebase("https://trenches.firebaseio.com/boards");
+	var ref = new Firebase(FBURL+"boards");
 	var sync = $firebase(ref);
 	$scope.boards = sync.$asArray();
 });
-app.controller('BoardCtrl', function($scope, $rootScope, $firebase, $routeParams) {
-	var ref = new Firebase("https://trenches.firebaseio.com/boards/"+$routeParams.boardId);
+app.controller('BoardCtrl', function($scope, $rootScope, FBURL, $firebase, $firebaseAuth, $routeParams) {
+	var ref = new Firebase(FBURL+"boards/"+$routeParams.boardId);
 	
+	$scope.boardId = $routeParams.boardId;
 	var settings = $firebase(ref.child('settings')).$asObject();
 	settings.$bindTo($scope, 'settings').then(function () {
 		$scope.states = settings.states.split(',');
@@ -31,21 +36,37 @@ app.controller('BoardCtrl', function($scope, $rootScope, $firebase, $routeParams
 	});
 	var cards = $firebase(ref.child('cards')).$asArray();
 	$scope.cards = cards;
+	$scope.auth = $firebaseAuth;
 	$scope.updateCard = function(id, update) {
-		var sync = $firebase(ref.child('cards').child(id));
-		sync.$update(update);
+		$firebaseAuth.$getCurrentUser().then(function(user) {
+			update.owned_by = user.username;
+			var sync = $firebase(ref.child('cards').child(id));
+			sync.$update(update);
+		});
 	}
 });
+app.controller('CardCtrl', function($scope, $rootScope, FBURL, $firebase, $routeParams) {
+	var ref = new Firebase(FBURL+"boards/"+$routeParams.boardId+'/cards/'+$routeParams.cardId);
 
+	$scope.boardId = $routeParams.boardId;
+	$scope.board = $firebase(new Firebase(FBURL+"boards/"+$routeParams.boardId+'/settings')).$asObject();
+	var card = $firebase(ref).$asObject();
+	card.$bindTo($scope, 'card').then(function() {
+		$rootScope.title = card.title + ' - Trenches';
+	});
+});
+
+// Custom directive for handling drag-and-drop
 
 var isIE9 = (navigator.appVersion.indexOf('MSIE 9.0') >= 0);
+
 app.directive('draggable', function($document) {
 	return function(scope, element, attr) {
 		element.attr('draggable', 'true');
 		if (isIE9) {
 			element.prepend("<a class='draghandle' draggable='true' id='"+scope.card.$id+"' href='#'><i class='fa fa-arrows'></i></a>");
 		}
-		element.bind('dragstart', function(e) {
+		element.bind('dragstart touchstart', function(e) {
 			e.dataTransfer.setData('text', scope.card.$id)
 		});
 	}
@@ -55,11 +76,11 @@ app.directive('droparea', function() {
 		element.bind('dragover', function(e) {
 			e.preventDefault();
 		});
-		element.bind('drop', function(e) {
+		element.bind('drop touchend', function(e) {
 			var id = e.dataTransfer.getData("text");
 			var state = e.target.getAttribute('state');
 			if (id && state) {
-				scope.updateCard(id, { state: state });
+				scope.updateCard(id, { state: state, owned_by: 'auth.user.username' });
 			} 
 			if (e.stopPropagation) e.stopPropagation();
 			if (e.preventDefault) e.preventDefault();
