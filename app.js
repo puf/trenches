1a6f186f9456b20a2939e4c22369d0b3fdc1a39e
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2014-09-02 12:18:29 -0400

    Better card layout, edit hours in board

--- app.js
+++ app.js
@@ -89,6 +89,43 @@ app.directive('droparea', function() {
 	}
 });
 
+
+// Custom directive for handling inline editing (from https://docs.angularjs.org/api/ng/type/ngModel.NgModelController)
+
+//angular.module('customControl', ['ngSanitize']).
+app.directive('contenteditable', ['$sce', function($sce) {
+    return {
+      restrict: 'A', // only activate on element attribute
+      require: '?ngModel', // get a hold of NgModelController
+      link: function(scope, element, attrs, ngModel) {
+      	console.log('link');
+        if(!ngModel) return; // do nothing if no ng-model
+
+        // Specify how UI should be updated
+        ngModel.$render = function() {
+          element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+        };
+
+        // Listen for change events to enable binding
+        element.on('blur keyup change', function() {
+          scope.$apply(read);
+        });
+        read(); // initialize
+
+        // Write data to the model
+        function read() {
+          var html = element.html();
+          // When we clear the content editable the browser leaves a <br> behind
+          // If strip-br attribute is provided then we strip this out
+          if( attrs.stripBr && html == '<br>' ) {
+            html = '';
+          }
+          ngModel.$setViewValue(html);
+        }
+      }
+    };
+  }]);
+
 // some non-angular helpers
 
 function addSampleCards() {
